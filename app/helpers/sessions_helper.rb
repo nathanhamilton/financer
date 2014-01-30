4eb8f1d57df87fbc1bf62c35b0840cfe23404bfd
nathanhamilton <esquireofthelord@gmail.com>
2014-01-30 16:51:49 -0600

    Create user sessions and dependencies

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,22 @@
+module SessionsHelper
+  def sign_in(user)
+    remember_token = User.new_remember_token
+    cookies[:remember_token] = {value: remember_token,
+                              expires: 10.minutes.from_now.utc}
+    user.update_attribute(:remember_token, User.encrypt(remember_token))
+    self.current_user = user
+  end
+
+  def sign_in?
+     !current_user.nil
+  end
+
+  def current_user=(user)
+    @current_user = user
+  end
+
+  def current_user
+    remember_token = User.encrypt(cookies[:remember_token])
+    @current_user ||= User.find_by(remember_token: remember_token)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Financer::Application.routes.draw do
   resources :users
-
+  resources :sessions, only: [:new, :create, :destroy]
   root to: 'static_pages#home'
 
   get '/about',   to: 'static_pages#about'
   get '/signup',  to: 'users#new'
+  get '/signin',  to: 'sessions#new'
+  get '/signout', to: 'sessions#destroy', via: 'delete'
 
 end
