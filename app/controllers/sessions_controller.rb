f30463b3fd32c497205c07e77fe6c8d11386d0cc
nathanhamilton <esquireofthelord@gmail.com>
2014-01-31 09:43:27 -0600

    Change title for pages and modify sessions for current_user to work

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def create
-    user = User.find_by_email(params[:session][:email].downcase)
+    user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       sign_in user
       redirect_to user
