9b2324535f991d60a510d00c2c4a39619f54f22c
nathanhamilton <esquireofthelord@gmail.com>
2014-02-04 11:46:33 -0600

    Add user_id index to banks

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140203181022) do
+ActiveRecord::Schema.define(version: 20140204174428) do
 
   create_table "banks", force: true do |t|
     t.string   "name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20140203181022) do
     t.datetime "updated_at"
     t.integer  "user_id"
   end
+
+  add_index "banks", ["user_id"], name: "index_banks_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "name"
