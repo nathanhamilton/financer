0f4ca7d70606658c42d647dca0147928899f1ee3
nathanhamilton <esquireofthelord@gmail.com>
2014-02-17 14:18:46 -0600

    Apply before filter to stop redirect loop

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  skip_before_action :signed_in_user
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
